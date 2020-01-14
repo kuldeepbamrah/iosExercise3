7f906879a2555db0a425b44c4641e1b6f0cc6372
kuldeepbamrah <boby.sbamrah@gmail.com>
2020-01-13 21:11:13 -0500

    Initial Commit

---
+++ iosExercise3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosExercise3
+//
+//  Created by MacStudent on 2020-01-13.
+//  Copyright © 2020 Kuldeep Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
