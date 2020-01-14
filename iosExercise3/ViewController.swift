066c35cfc7da69ed8e26929d1958de444f94d918
kuldeepbamrah <boby.sbamrah@gmail.com>
2020-01-13 21:13:24 -0500

    initial commit

--- iosExercise3/ViewController.swift
+++ iosExercise3/ViewController.swift
@@ -8,10 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
-    override func viewDidLoad() {
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+    
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
